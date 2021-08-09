e150fd32697e10f5199634bc6b675e3e882b7424
Jonathan Wills <jonathan_wills@icloud.com>
2021-08-09 22:43:25 +0900

    💄 Import global scss file

--- pages/_app/index.tsx
+++ pages/_app/index.tsx
@@ -2,6 +2,10 @@
 import { AppProps } from 'next/app';
 // #endregion Global Imports
 
+// #region Local Imports
+import '@Static/scss/index.scss';
+// #endregion Local Imports
+
 /* eslint-disable react/jsx-props-no-spreading */
 function App({ Component, pageProps }: AppProps): JSX.Element {
     return <Component {...pageProps} />;
