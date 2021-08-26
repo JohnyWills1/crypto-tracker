d172112dc27dfa97831d43e0ad7e0e53c9835a92
Jonathan Wills <jonathan_wills@icloud.com>
2021-08-26 22:31:21 +0900

    ✨ Add get initial data function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,30 @@
+// #region Global Imports
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
+import { AxiosPromise } from 'axios';
+// #endregion Global Imports
+
+// #region Local Imports
+import { APIError, fetchCryptos } from '@Services/API/index';
+// #endregion Local Imports
 
 function Home(): JSX.Element {
+    const [initialData, setInitialData] = useState<AxiosPromise<Crypto>>();
+
+    useEffect(() => {
+        async function getInitData(): Promise<void> {
+            getInitData()
+                .then((res: any) => {
+                    setInitialData(res.data);
+                })
+                .catch((e: APIError) => {
+                    console.error(e);
+                });
+        }
+
+        getInitData();
+    }, []);
+
     return (
         <div className="ct--home">
             <Head>
